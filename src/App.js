75c399a15d61d664bf1a82a0b7db73d0300d8786
bananakiu <leandrenoelkiu@gmail.com>
2021-08-28 13:40:41 +0800

    add login api logic (in progress)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Login from "./Components/Login/Login";
+import axios from 'axios';
+import { useEffect } from 'react';
+
+export const API = "206.189.91.54";
 
 function App() {
   return (
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import axios from "axios";
+import { API } from '../../App'
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-  const [result, setResult] = useState("");
-  const onSubmit = (data) => setResult(data);
+  const [ user, setUser ] = useState({});
+
+  const onSubmit = (data) => {
+    // create user obj
+    let loggedInUser = {
+      "email": data.username,
+      "password": data.password,
+    }
+    // save user obj
+    setUser(loggedInUser);
+
+    // POST to server
+    axios.post(
+      `https://${API}/api/v1/auth/sign_in`,
+      loggedInUser,
+    ).then((response) => {
+      console.log(response)
+    }).catch((error) => {
+      console.error(error);
+    })
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} placeholder="Username" />
+      <input {...register("username")} placeholder="Username" />
       <input {...register("password")} type="password" placeholder="Password" />
 
-      <p>{JSON.stringify(result)}</p>
+      <p>{JSON.stringify(user)}</p>
       <button>Login</button>
     </form>
   );
