e190546baaee3e15e7c01fac113c16b8613c1f86
bananakiu <leandrenoelkiu@gmail.com>
2021-08-28 10:30:55 +0800

    setup axios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="text-9xl">
           Hello World
         </p>
         <a
