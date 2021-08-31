24c540ff0cbddd58183aa53bb1cb5169f19b246a
Ivan Bautista <ivankevinbautista@gmail.com>
2021-08-31 18:04:01 +0800

    Add in states for login form, signup form, and userdashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import Login from "./components/login/Login";
 import Signup from "./components/login/Signup";
-
+import UserDashboard from "./components/userDashboard/UserDashboard";
+import React, {useState} from "react";
 export const API = "http://206.189.91.54";
 
+
 function App() {
+  const [logInForm, toggleLoginForm] = useState(true);
+  const [signUpForm, toggleSignUpForm] = useState(false);
+  const [userDashboard, toggleUserDashboard] = useState(false);
+
+
+const handleLogin = () => {
+  /* TEMP HANDLER for user/channel view || Can be renamed to anything */
+  // toggleUserDashboard(true); 
+  toggleLoginForm(false);
+  toggleUserDashboard(true);
+}
+
+const handleSignup = () => {
+  toggleLoginForm(false);
+  toggleSignUpForm(true);
+}
+
+const handleCreateAccount = () => {
+  toggleUserDashboard(true);
+  toggleSignUpForm(false);
+}
+
+
   return (
-    <div className="App">
-      {/* <Login /> */}
-      <Signup />
+    <div>
+      {logInForm && <Login handleLogin={handleLogin} handleSignup={handleSignup}/>}
+      {userDashboard && <UserDashboard />}
+      {signUpForm && <Signup handleLogin={handleLogin} handleCreateAccount={handleCreateAccount}/>}
     </div>
   );
 }
 
 export default App;
+
