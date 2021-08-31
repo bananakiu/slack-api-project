9da47d4e423aa6486e14d595fba6055c2297d220
Ivan Bautista <ivankevinbautista@gmail.com>
2021-08-31 18:06:29 +0800

    Add in tentative userDashboard to test functionality

---
+++ src/components/userDashboard/UserDashboard.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API } from '../../App'
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+
+const UserDashboard = () => {
+
+ 
+return (
+    <div className="py-6">
+            <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-2xl">
+                <div className="w-full p-8 lg:w-1/2">
+                    <h2 className="text-2xl font-semibold text-gray-700 text-center">Test component for User Dashboard</h2>
+                </div>
+            </div>
+    </div>
+)}
+
+export default UserDashboard;
