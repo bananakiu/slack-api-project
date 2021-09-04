0280b319d2215dee7aff7dc2dd7b6f2d53ff1906
Ivan Bautista <ivankevinbautista@gmail.com>
2021-09-04 14:09:34 +0800

    Change bg to gray

--- src/components/userDashboard/UserDashboard.jsx
+++ src/components/userDashboard/UserDashboard.jsx
@@ -59,7 +59,7 @@ const UserDashboard = (props) => {
 
     return <>
         <div className="py-6">
-                <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-2xl">
+                <div className="flex bg-gray-100 rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-2xl">
                     <div className="w-full p-8 lg:w-1/2">
                         <h2 className="text-2xl font-semibold text-gray-700 text-center">Test component for User Dashboard</h2>
                         <GetAllChannels allChannels={allChannels}/>
