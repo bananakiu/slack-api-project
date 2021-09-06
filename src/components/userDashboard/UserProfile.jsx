802eb5e97bd562153933da65c84d44349ec59c84
bananakiu <leandrenoelkiu@gmail.com>
2021-09-06 17:27:36 +0800

    change class to className

--- src/components/userDashboard/UserProfile.jsx
+++ src/components/userDashboard/UserProfile.jsx
@@ -6,7 +6,7 @@ const UserProfile = (props) => {
         <>
             <div className="flex flex-col items-center justify-center h-full w-50 bg-gray-100 rounded-2xl overflow-hidden">
                 
-            <img class="rounded-full border border-red-500 shadow-inner p-3 mt-2" src="https://randomuser.me/api/portraits/men/22.jpg" alt="user image" />
+            <img className="rounded-full border border-red-500 shadow-inner p-3 mt-2" src="https://randomuser.me/api/portraits/men/22.jpg" alt="user image" />
                 <div className="text-base px-2 py-2 font-bold"> {props.loginUser.uid}</div>
                 <div className="text-sm pb-2 font-bold"> {props.loginUser.id}</div>
             </div>
