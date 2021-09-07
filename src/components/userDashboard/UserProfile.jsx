699fc1cb6dd3a0fa0777e838d338c1a8e60e9200
Ivan Bautista <ivankevinbautista@gmail.com>
2021-09-07 19:11:00 +0800

    Change styling to prevent clipping on image

--- src/components/userDashboard/UserProfile.jsx
+++ src/components/userDashboard/UserProfile.jsx
@@ -7,11 +7,10 @@ const UserProfile = () => {
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center h-full w-50 bg-gray-100 rounded-2xl overflow-hidden">
+            <div className="flex flex-col items-center justify-center h-30 w-50 mb-3 bg-gray-100 rounded-2xl object-scale-down">
                 
-            <img className="rounded-full border border-red-500 shadow-inner p-3 mt-2" src="https://randomuser.me/api/portraits/men/22.jpg" alt="user image" />
+            <img className="rounded-full border border-red-300 p-3 mt-2" src="https://randomuser.me/api/portraits/men/22.jpg" alt="user image" />
                 <div className="text-base px-2 py-2 font-bold"> {loginUser.uid}</div>
-                <div className="text-sm pb-2 font-bold"> {loginUser.id}</div>
             </div>
         </>
     )
