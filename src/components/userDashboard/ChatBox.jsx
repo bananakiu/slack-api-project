b09e21206e4b445e8383c5d60185bf6107e1e77c
bananakiu <leandrenoelkiu@gmail.com>
2021-09-09 04:29:56 +0800

    remove doubled class

--- src/components/userDashboard/ChatBox.jsx
+++ src/components/userDashboard/ChatBox.jsx
@@ -47,7 +47,7 @@ const ChatBox = () => {
     <>
       <div className='flex-none border-2 h-16'>
         <form onSubmit={handleSubmit(onSubmit)} className='flex justify-between w-full'>
-          <input type="text" placeholder="Enter your message here" name="message" {...register('message')} className='w-full m-1 mb-2 py-2 px-3 w-full border-gray-300 border-2 rounded-lg' />
+          <input type="text" placeholder="Enter your message here" name="message" {...register('message')} className='w-full m-1 mb-2 py-2 px-3 border-gray-300 border-2 rounded-lg' />
           <input type="submit" className='max-w-max bg-blue-800 text-white border-blue-700 border-2 rounded-lg m-1 mb-2 py-2 px-3 cursor-pointer' />
         </form>
       </div>
