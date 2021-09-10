608d76d122f41789f46344a3b384611cb2882611
bananakiu <leandrenoelkiu@gmail.com>
2021-09-11 00:21:40 +0800

    cleanup login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -25,8 +25,6 @@ const Login = () => {
       "password": data.password,
     }
 
-    console.log(loggedInUser)
-
     // handle errors
     errorList = handleErrors(loggedInUser, errorList);
 
@@ -83,9 +81,6 @@ return (
               <div className="mb-1 h-14 w-14"> 
                 <img src={Fireside}/>
               </div>
-              {/* <h3 className="mb-3 text-center justify-center align-center font-semibold text-2xl text-gray-800 font-bold">
-                Sign In{" "}
-              </h3> */}
               <p className="mt-1 italic text-gray-500 text-center">
                 "A chat a day, keeps the loneliness away"
               </p>
@@ -176,39 +171,3 @@ return (
 };
 
 export default Login;
-
-// return (
-//   <form onSubmit={handleSubmit(onSubmit)}>
-//       <div className="py-6">
-//         <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-2xl">
-//           <div className="hidden lg:block lg:w-1/2 bg-cover" style={{backgroundImage: "url('https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG9mZmljZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')"}}></div>
-//           <div className="w-full p-8 lg:w-1/2">
-//             <h2 className="text-2xl font-semibold text-gray-700 text-center">Slack App Logo here</h2>
-//             <p className="text-xl text-gray-600 text-center">Welcome back!</p>
-//             {errors.length > 0 &&
-//               <div className="mt-2">
-//                 <ErrorDisplay errors={errors}/>
-//               </div>
-//             }
-//             <div className="mt-4">
-//               <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-//               <input {...register("username")} placeholder="Username" className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"/>
-//             </div>
-//             <div className="mt-4">
-//               <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-//               <input {...register("password")} type="password" placeholder="Password" className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"/>
-//             </div>
-//             <div className="mt-8">
-//               <button className="bg-purple-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-purple-400">Login</button>
-//             </div>
-//             <div className="mt-8">
-//                 <button type="button" className="bg-purple-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-purple-400" onClick={() => openPage("signup")}>Signup</button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </form>
-//     );
-//   };
-
-// export default Login;
