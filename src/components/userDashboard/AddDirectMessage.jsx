86ae76fb3fb21a45dab99871fe0bbd6ca1cce122
bananakiu <leandrenoelkiu@gmail.com>
2021-09-11 04:45:19 +0800

    change chat states onChange instead of onSubmit for the select a user to DM form

--- src/components/userDashboard/AddDirectMessage.jsx
+++ src/components/userDashboard/AddDirectMessage.jsx
@@ -1,28 +1,22 @@
-import React, { useState, useContext } from 'react';
-import Modal from '../common/Modal';
-import ErrorDisplay from '../common/ErrorDisplay';
-import { useForm, Controller } from 'react-hook-form';
+import React, { useState, useContext, useEffect } from 'react';
 import Select from 'react-select';
 import { API, StatesContext } from '../../App';
 
 const AddDirectMessage = () => {
-    const { register, handleSubmit, control } = useForm();
-
     const { 
-        loginUser, allUsers, 
-        currentChatId, setCurrentChatId,
-        currentChatType,setCurrentChatType,
-        currentChatName,setCurrentChatName} = useContext(StatesContext);
+        loginUser,
+        allUsers, 
+        currentChatId,
+        setCurrentChatId,
+        currentChatType,
+        setCurrentChatType,
+        currentChatName,
+        setCurrentChatName,
+        setCurrentChatMembers,
+    } = useContext(StatesContext);
 
-    const onSubmit = (data) => {
-        if(data.user_id?.label !== undefined && data.user_id?.value !== undefined) {
-            // console.log(data)
-            setCurrentChatName(data.user_id.label);
-            setCurrentChatType("User");
-            setCurrentChatId(data.user_id.value);
-        }
-    }
-
+    const [answer, setAnswer] = useState([])
+    
     let allUsersOptions = allUsers.map((indivUser) => {
         return {
             value: indivUser.id,
@@ -30,48 +24,38 @@ const AddDirectMessage = () => {
         }
     })
 
+    useEffect(() => {
+        setCurrentChatType("User")
+        setCurrentChatId(answer.value)
+        setCurrentChatName(answer.label)
+        setCurrentChatMembers([])
+    }, [answer])
+
     return (
-            <form onSubmit={handleSubmit(onSubmit)} className="
-            flex flex-col items-center justify-center flex-grow-0
-            h-20 mt-2
+        <form className="
+        flex flex-col items-center justify-center flex-grow-0
+        h-20 mt-2
+        ">
+            <h1 className="
+            font-semibold text-lg self-start
             ">
-                <h1 className="
-                font-semibold text-lg self-start
-                ">Direct Messages</h1>
-                {/* {errors.length>0 &&
-                    <div className="
-                    flex flex-col justify-center items-center
-                    w-full mb-4
-                    ">
-                        <ErrorDisplay errors={errors}/>
-                    </div>
-                } */}
-                <div className="
-                flex flex-col justify-center items-center
-                w-full
-                ">
-                    <Controller
-                        name="user_id"
-                        isClearable
-                        control={control}
-                        render={({ field }) => (
-                            <Select
-                                {...field}
-                                options={allUsersOptions}
-                                isSearchable
-                                required
-                                placeholder="Select from this list"
-                                className="w-full text-left"
-                                setValue={handleSubmit(onSubmit)}
-                                // onChange={()=>handleSubmit(onSubmit)}
-                                menuPlacement="auto"
-                                // onChange={handleSubmit(onSubmit)}
-                                // styles={}
-                            />
-                        )}
-                    />
-                </div>
-            </form>
+                Direct Messages
+            </h1>
+            <div className="
+            flex flex-col justify-center items-center
+            w-full
+            ">
+                <Select
+                    options={allUsersOptions}
+                    isSearchable
+                    required
+                    placeholder="Select from this list"
+                    className="w-full text-left"
+                    menuPlacement="auto"
+                    onChange={setAnswer}
+                />
+            </div>
+        </form>
     )
 }
 
